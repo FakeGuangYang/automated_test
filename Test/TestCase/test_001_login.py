54b68e94a9ed0f3b8803b4686a5d2bd1c16c8f2d
guangyang219579 <guangyang219579@sohu-inc.com>
2022-06-07 15:11:38 +0800

    add chromedriver service.

--- Test/TestCase/test_001_login.py
+++ Test/TestCase/test_001_login.py
@@ -16,7 +16,7 @@ chrome_options.add_argument('blink-settings=imagesEnabled=false')
 @pytest.mark.parametrize(("username", "password", "status"), data)
 class TestLogin():
     def setup(self):
-        self.driver = webdriver.Chrome(executable_path=chrome_webdriver_path, chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.driver.get(url)
