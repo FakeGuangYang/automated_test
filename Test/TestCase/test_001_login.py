57f18c168261f24db103a4174a484ce3d311081b
guangyang219579 <guangyang219579@sohu-inc.com>
2022-06-07 17:10:13 +0800

    add chromedriver service.

--- Test/TestCase/test_001_login.py
+++ Test/TestCase/test_001_login.py
@@ -1,22 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import pytest
 from Test.PageObject import login_page
 from Common.parse_csv import parse_csv
+from selenium.webdriver.common.by import By
 
-data = parse_csv("Data/test_001_login.csv")
+data = parse_csv("../../Data/test_001_login.csv")
 url = "https://sso.sohu-inc.com/login?service=http://opt.mrd.sohuno.com/operation/ssoValidate?returnUrl=/"
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('blink-settings=imagesEnabled=false')
 
 
 @pytest.mark.parametrize(("username", "password", "status"), data)
 class TestLogin():
     def setup(self):
-        self.driver = webdriver.Chrome()
-        # self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Firefox(options=chrome_options)
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.driver.get(url)
@@ -28,6 +29,7 @@ class TestLogin():
             assert text == '登录失败！请输入有效的用户名/密码。'
         elif status == '1':
             text = login_page.LoginOper(self.driver).get_login_name()
+            print(self.driver.find_element(By.CLASS_NAME, 'email').text)
             assert text == "guangyang219579@sohu-inc.com"
             # text = self.driver.page_source
             # assert text == "guangyang219579@sohu-inc.com"
