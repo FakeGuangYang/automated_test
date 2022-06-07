5cc88c6be5de22c413d11025be0d62025113cc40
guangyang219579 <guangyang219579@sohu-inc.com>
2022-06-07 18:21:12 +0800

    automated test for login page

--- Test/TestCase/test_001_login.py
+++ Test/TestCase/test_001_login.py
@@ -3,10 +3,10 @@ from selenium.webdriver.chrome.options import Options
 import pytest
 from Test.PageObject import login_page
 from Common.parse_csv import parse_csv
-from selenium.webdriver.common.by import By
 
 data = parse_csv("Data/test_001_login.csv")
 url = "https://sso.sohu-inc.com/login?service=http://opt.mrd.sohuno.com/operation/ssoValidate?returnUrl=/"
+# need to add some arguments when running on Linux.
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
@@ -17,7 +17,6 @@ chrome_options.add_argument('blink-settings=imagesEnabled=false')
 class TestLogin():
     def setup(self):
         self.driver = webdriver.Chrome(options=chrome_options)
-        # self.driver = webdriver.Firefox(options=chrome_options)
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.driver.get(url)
@@ -27,12 +26,10 @@ class TestLogin():
         if status == '0':
             text = login_page.LoginOper(self.driver).get_login_failed_info()
             assert text == '登录失败！请输入有效的用户名/密码。'
+        # TODO: cannot find login_name using "find_element()", need to understand why.
         elif status == '1':
-            text = self.driver.find_element(By.CSS_SELECTOR, '#leftsidebar > div.user-info > div.info-container > div.email').text
-            assert "guangyang219579@sohu-inc.com" == text
-            # text = self.driver.page_source
-            # assert text == "guangyang219579@sohu-inc.com"
-            # print(text)
+            page_source = self.driver.page_source
+            assert "guangyang219579@sohu-inc.com" in page_source
         else:
             print("Wrong status.")
 
